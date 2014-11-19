db27e64839f064fd1cff3e0120caaf8a735878da
Radoslav Slavkov <R.Slavkov951@gmail.com>
2014-11-19 11:49:06 +0200

    visual update
    Направих визуален Update. Излезе еrror при поставяне на първото кубче.
    Трябва да се направи алгоритам за (win,lose,draw).

--- Game/4-in-a-line/scripts/game.js
+++ Game/4-in-a-line/scripts/game.js
@@ -6,12 +6,14 @@ var canvas,
     player,
     turn,
     chipsPlaced,
+    chipsPositiony,
+    chipsPositionx,
     startRow,
     mouseX,
     mouseY;
 
 
-function draw(){ //Everything on screen is drawn here.
+function draw() { //Everything on screen is drawn here.
 
     ctx.save();
     clear();
@@ -20,120 +22,140 @@ function draw(){ //Everything on screen is drawn here.
     ctx.restore;
     window.requestAnimationFrame(draw);
 }
-function drawBoard(){
+function drawBoard() {
     ctx.strokeStyle = '#000112';
     drawHead();
     mainBoard();
     drawFoot();
-
-
-    function drawHead(){ //The top part of the board.
-        ctx.strokeRect(0, 0, cWidth, 50);
-        markSpot(); //Marks where player will take turn.
-
-        function markSpot(){ //Marks where player will take his turn..
-            if (player.x >= 0 && player.x <= 52) { //1
-                placeMark(-1);
-            }else if (player.x >= 53 && player.x <= 108){ //2
-                placeMark(55);
-            }else if (player.x >= 109 && player.x <= 164){ //3
-                placeMark(111);
-            }else if (player.x >= 165 && player.x <= 220) { //4
-                placeMark(167);
-            }else if (player.x >= 221 && player.x <= 275) { //5
-                placeMark(223);
-            }else if (player.x >= 270 && player.x <= 332) { //6
-                placeMark(279);
-            }else if (player.x >= 333 && player.x <= 388) { //7
-                placeMark(335);
-            }else if (player.x >= 389 && player.x <= 443) { //8
-                placeMark(391);
-            }else if (player.x >= 444 && player.x <= 501) { //9
-                placeMark(447);
-            }else if (player.x >= 502 && player.x <= 555) { //10
-                placeMark(503);
-            }
-            function placeMark(a){ //Fills the above marked spot based on player.
-                ctx.strokeRect(a+1,1,50,49);
-                if (turn == 1) { //Draws a square.
+    if (gameStart) {
+        drawPlayers();
+        
+    }
+    function drawPlayers() {
+        var row="";
+        for (var row = 0; row < 5; row++) {
+            for (var col = 0; col < 10; col++) {
+                if (chipsPlaced[row][col]==1) {
                     ctx.fillStyle = player.p1color;
-                    ctx.fillRect(a+6, 5, 40, 40);
-                } else if (turn == 2) { //Draws a circle.
-                    ctx.beginPath();
+                    ctx.fillRect(chipsPositionx[col]+5, chipsPositiony[row]+5, 40, 40);
+                }
+                if (chipsPlaced[row][col] == 2) {
                     ctx.fillStyle = player.p2color;
-                    ctx.arc(a+26, 25, 40 / 2, 0, 2 * Math.PI, false);
-                    ctx.fill();
-                    ctx.closePath();
+                    ctx.fillRect(chipsPositionx[col]+5, chipsPositiony[row]+5, 40, 40);
                 }
             }
         }
     }
-    function mainBoard(){ //Draws grid in the middle of the screen. If you want you can delete it and draw whatever you want...
+
+    function drawHead() { //The top part of the board.
+        ctx.strokeRect(0, 0, cWidth, 50);
+        markSpot(); //Marks where player will take turn.
+
+        function markSpot() { //Marks where player will take his turn..
+            if (player.x >= 0 && player.x <= 52) { //1
+                placeMark(-1);
+            } else if (player.x >= 53 && player.x <= 108) { //2
+                placeMark(55);
+            } else if (player.x >= 109 && player.x <= 164) { //3
+                placeMark(111);
+            } else if (player.x >= 165 && player.x <= 220) { //4
+                placeMark(167);
+            } else if (player.x >= 221 && player.x <= 275) { //5
+                placeMark(223);
+            } else if (player.x >= 270 && player.x <= 332) { //6
+                placeMark(279);
+            } else if (player.x >= 333 && player.x <= 388) { //7
+                placeMark(335);
+            } else if (player.x >= 389 && player.x <= 443) { //8
+                placeMark(391);
+            } else if (player.x >= 444 && player.x <= 501) { //9
+                placeMark(447);
+            } else if (player.x >= 502 && player.x <= 555) { //10
+                placeMark(503);
+            }
+            function placeMark(a) { //Fills the above marked spot based on player.
+                ctx.strokeRect(a + 1, 1, 50, 49);
+                if (turn == 1) { //Draws a square.
+                    ctx.fillStyle = player.p1color;
+                    ctx.fillRect(a + 6, 5, 40, 40);
+                } else if (turn == 2) { //Draws a circle.
+                    ctx.fillStyle = player.p2color;
+                    ctx.fillRect(a + 6, 5, 40, 40);
+                }
+            }
+        }
+    }
+    function mainBoard() { //Draws grid in the middle of the screen. If you want you can delete it and draw whatever you want...
         for (var row = 0; row < 5; row++) {
             for (var col = 0; col < 10; col++) {
+                ctx.fillStyle = "#FFD700";
+                ctx.fillRect(chipsPositionx[col], chipsPositiony[row], 50, 50);
+                ctx.fillStyle = "#659CEF";
+                ctx.fillRect(chipsPositionx[col]+5, chipsPositiony[row]+5, 40, 40);
                 ctx.strokeRect(56 * col, 55 + 56 * row, 50, 50);
             }
         }
     }
-    function drawFoot(){ //Used for info display.
-        ctx.strokeRect(0,335, cWidth,50);
+    function drawFoot() { //Used for info display.
+        ctx.strokeRect(0, 335, cWidth, 50);
 
-        if (!gameStart){
+        if (!gameStart) {
             ctx.fillStyle = 'black';
             ctx.font = '40px Arial bold'
             //ctx.textAlign = 'start'; //Aligns text at the bottom looks ugly as fk -.-
             //ctx.textBaseline = 'middle';
-            ctx.fillText('Click to start',0,370,cWidth);
+            ctx.fillText('Click to start', 0, 370, cWidth);
         }
-        else{
-            if(turn==1){ //Somehow takes into account player color and uses it.
+        else {
+            if (turn == 1) { //Somehow takes into account player color and uses it.
                 ctx.font = '40px Arial bold'
                 //ctx.textAlign = 'start';
-                ctx.fillText('It`s Player 1`s turn',0,370,cWidth);
+                ctx.fillText('It`s Player 1`s turn', 0, 370, cWidth);
             }
-            else if (turn==2){ //Somehow takes into account player color and uses it.
+            else if (turn == 2) { //Somehow takes into account player color and uses it.
                 ctx.font = '40px Arial bold'
                 //ctx.textAlign = 'start';
-                ctx.fillText('It`s Player 2`s turn',0,370,cWidth);
-            }else{
+                ctx.fillText('It`s Player 2`s turn', 0, 370, cWidth);
+            } else {
                 ctx.font = '40px Arial bold'
                 //ctx.textAlign = 'start';
-                ctx.fillText('Click inside the game to start.',0,370,cWidth);
+                ctx.fillText('Click inside the game to start.', 0, 370, cWidth);
             }
         }
     }
 }
-function drop(){
+function drop() {
+
     //If you click between x and x and you drop a chip at N col at the lowest free row.
     if (player.x >= 0 && player.x <= 52) { //1
         placeChip(1);
-    }else if (player.x >= 53 && player.x <= 108){ //2
+    } else if (player.x >= 53 && player.x <= 108) { //2
         placeChip(2);
-    }else if (player.x >= 109 && player.x <= 164){ //3
+    } else if (player.x >= 109 && player.x <= 164) { //3
         placeChip(3);
-    }else if (player.x >= 165 && player.x <= 220) { //4
+    } else if (player.x >= 165 && player.x <= 220) { //4
         placeChip(4);
-    }else if (player.x >= 221 && player.x <= 275) { //5
+    } else if (player.x >= 221 && player.x <= 275) { //5
         placeChip(5);
-    }else if (player.x >= 270 && player.x <= 332) { //6
+    } else if (player.x >= 270 && player.x <= 332) { //6
         placeChip(6);
-    }else if (player.x >= 333 && player.x <= 388) { //7
+    } else if (player.x >= 333 && player.x <= 388) { //7
         placeChip(7);
-    }else if (player.x >= 389 && player.x <= 443) { //8
+    } else if (player.x >= 389 && player.x <= 443) { //8
         placeChip(8);
-    }else if (player.x >= 444 && player.x <= 501) { //9
+    } else if (player.x >= 444 && player.x <= 501) { //9
         placeChip(9);
-    }else if (player.x >= 502 && player.x <= 555) { //10
+    } else if (player.x >= 502 && player.x <= 555) { //10
         placeChip(10);
     }
 
     //Adds chips to the matrix and prevents the player from placing chips
-    function placeChip (col){
-        if (chipsPlaced[4][col-1]!=0){ //prevents overflow in rows.
+    function placeChip(col) {
+        if (chipsPlaced[4][col - 1] != 0) { //prevents overflow in rows.
 
             //Can add text here. If u want.
 
-        }else {
+        } else {
             while (chipsPlaced[startRow][col - 1] != 0 && startRow < 6) { //Looks for an empty column in current row.
                 startRow++;
             }
@@ -150,43 +172,41 @@ function drop(){
                 }
             }
             //Swaps turns.
-            if(turn==1){
-                turn=2;
+            if (turn == 1) {
+                turn = 2;
             }
-            else{
-                turn=1;
+            else {
+                turn = 1;
             }
         }
 
     }
 
 }
-function clear(){
-    ctx.clearRect(0,0,cWidth,cHeight);
+function clear() {
+    ctx.clearRect(0, 0, cWidth, cHeight);
 }
-function Player(){ //New player thingy.
+function Player() { //New player thingy.
     this.x = mouseX;
     this.y = mouseY;
 
     this.width = 40;
     this.height = 40;
-    this.p1color = '#000eff';
-    this.p2color = '#ffe000';
-    this.draw = function(){ //Draws circle/square at mouse cursor.
-        if (turn==1){ //player 1 turn;
+    this.p1color = '#000eff'; //Blue
+    this.p2color = '#FF0000'; //Red
+    this.draw = function () { //Draws circle/square at mouse cursor.
+        if (turn == 1) { //player 1 turn;
             ctx.fillStyle = this.p1color; //player 1 color
-            ctx.fillRect(this.x-this.width/2, this.y-this.height/2, this.width, this.height);
+            ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+
         }
-        else if (turn==2){ //player 2 turn;
-            ctx.beginPath();
-            ctx.fillStyle = this.p2color; //player 2 color
-            ctx.arc(this.x, this.y, this.width/2, 0,2*Math.PI, false);
-            ctx.fill();
-            ctx.closePath();
+        else if (turn == 2) { //player 2 turn;
+            ctx.fillStyle = this.p2color; //player 1 color
+            ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
         }
     }
 }
-function init(){ //Everything is initialised here.
+function init() { //Everything is initialised here.
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     cWidth = canvas.width;
@@ -195,32 +215,34 @@ function init(){ //Everything is initialised here.
 
     startRow = 0; //used for calculating the rows. Don't touch.
 
-    chipsPlaced =   [[0,0,0,0,0,0,0,0,0,0], //The board represented in a matrix. If player 1 places a chip 0 turns into a 1 if player 2 places a chip 0 turns into a 2.
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0]];
+    chipsPlaced = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //The board represented in a matrix. If player 1 places a chip 0 turns into a 1 if player 2 places a chip 0 turns into a 2.
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
+    chipsPositiony = [279, 223, 167, 111, 55];
+    chipsPositionx = [0, 56, 112, 168, 224,280,336,392,448,504];
     gameStart = false; //Used for "Click to start game" msg at start of game.
 
     player = new Player(); //Initialise the player.
 
-    document.addEventListener('mousemove',function(){ //Used to get mouse position and set "player" position at cursor.
-        mouseX = event.clientX-canvas.offsetLeft;
-        mouseY = event.clientY-canvas.offsetTop;
+    document.addEventListener('mousemove', function () { //Used to get mouse position and set "player" position at cursor.
+        mouseX = event.clientX - canvas.offsetLeft;
+        mouseY = event.clientY - canvas.offsetTop;
 
         player.x = mouseX;
         player.y = mouseY;
     });
-    document.addEventListener('mousedown',function(){ //Starts game on click.
+    document.addEventListener('mousedown', function () { //Starts game on click.
         gameStart = true;
     });
-    document.addEventListener('mousedown',drop); //Calls drop function on mouse click.
+    document.addEventListener('mousedown', drop); //Calls drop function on mouse click.
     draw(); //Calls draw method.
 }
 
 
-window.addEventListener('load',init); //Starts everything
+window.addEventListener('load', init); //Starts everything
 
 
 
